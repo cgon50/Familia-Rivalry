b2c2c7224d6f8baec530e1a83141de64659b83f3
Zachary May <zmay@utexas.edu>
2019-11-11 22:05:51 -0600

    Set up answer box to perform guess, and some brief UI changes

--- app/src/main/java/com/example/familiarivalry/MainActivity.kt
+++ app/src/main/java/com/example/familiarivalry/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.familiarivalry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        answerET.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                this.performGuess()
+            }
+            true
+        }
+
+        guessBut.setOnClickListener {
+            performGuess()
+        }
+    }
+
+    private fun performGuess() {
+        hideKeyboard()
+    }
+
+    fun hideKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        //Find the currently focused view, so we can grab the correct window token from it.
+        var view = currentFocus
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = View(this)
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0);
     }
 }
