448ec426a6743299b34e265474c288c3ecf77e22
Zachary May <zmay@utexas.edu>
2019-11-30 21:23:22 -0600

    why does this sorta almost work? FIND OUT NEXT TIME

--- app/src/main/java/com/example/familiarivalry/P2pFragment.kt
+++ app/src/main/java/com/example/familiarivalry/P2pFragment.kt
@@ -1,10 +1,8 @@
 package com.example.familiarivalry
 
-import android.app.Activity
-import android.content.Context
-import android.net.Uri
 import android.net.wifi.p2p.WifiP2pDevice
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -35,32 +33,41 @@ class P2pFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         var p2pView = inflater.inflate(R.layout.fragment_p2p, container, false)
-
+        Log.d("ASDF","CREATION")
         helper = WifiP2PHelper(context)
+        helper.requestConnInfo()
         listView = p2pView.findViewById<ListView>(R.id.peerList)
         listView.setOnItemClickListener { parent,_,position,_ ->
             val item = parent.getItemAtPosition(position) as WifiP2pDevice
             helper.connectToPeer(item)
         }
         helper.setConnectInfoListener { address, isGroupOwner, groupFormed ->
+            Log.d("ASDF","ASDF")
             println("========\nAddress:$address\nisGroupOwner:$isGroupOwner\ngroupFormed:$groupFormed\n========")
         }
         helper.setConnectListener(object : WifiP2PHelper.ConnectListener{
+
             override fun connectState(state: Boolean) {
+                Log.d("ASDF","CONNECTSTATE")
                 println("Connect State: $state")
             }
 
             override fun connectDone(state: Boolean) {
+                Log.d("ASDF","CONNECTDONE")
                 println("Connect Done: $state")
             }
 
         })
         helper.setPeerListener {
-            listView.adapter = ArrayAdapter<WifiP2pDevice>(context!!, android.R.layout.simple_expandable_list_item_1, it)
+            Log.d("ASDF","LISTENER")
+            if (it != null)
+                listView.adapter = ArrayAdapter<WifiP2pDevice>(context!!, android.R.layout.simple_expandable_list_item_1, it)
         }
 
+        helper.easyStart()
         helper.startDiscovery()
 
+//        helper.requestConnInfo()
         return p2pView
     }
 
